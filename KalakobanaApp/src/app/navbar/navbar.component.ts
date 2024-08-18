5038e87540ea1748373f9b841df63c8b80a37c9e
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-18 21:50:18 +0400

    navbar started

---
+++ KalakobanaApp/src/app/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  isMenuOpen = false;
+  loggedIn = signal(true);
+  profileMenu = false;
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  toggleProfileMenu() {
+    this.profileMenu = !this.profileMenu;
+    console.log(this.profileMenu);
+
+  }
+
+}

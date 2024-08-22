69a189c7a10172f9f472c2105a434a36b83880ce
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-22 20:49:52 +0400

    game-hub done

--- KalakobanaApp/src/app/app.routes.ts
+++ KalakobanaApp/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ContactComponent } from './contact/contact.component';
+import { GameHubComponent } from './game-hub/game-hub.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
     { path: 'contact', component: ContactComponent },
+    { path: 'game-hub', component: GameHubComponent },
 ];
---
+++ KalakobanaApp/src/app/game-hub/game-hub.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-hub',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-hub.component.html',
+  styleUrl: './game-hub.component.css'
+})
+export class GameHubComponent {
+
+}

247a3999c437a7504d0bdf014b9affd6a649fb1c
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-22 18:50:09 +0400

    pluses

---
+++ KalakobanaApp/src/app/pluses/pluses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pluses',
+  standalone: true,
+  imports: [],
+  templateUrl: './pluses.component.html',
+  styleUrl: './pluses.component.css'
+})
+export class PlusesComponent {
+
+}

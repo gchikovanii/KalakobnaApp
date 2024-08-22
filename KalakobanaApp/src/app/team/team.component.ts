9f4ee7f964765231b3f086c628ff4f1ba210d1d7
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-22 19:11:53 +0400

    team done

--- KalakobanaApp/src/app/team/team.component.ts
+++ KalakobanaApp/src/app/team/team.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faInstagram, faLinkedin, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './team.component.html',
   styleUrl: './team.component.css'
 })
 export class TeamComponent {
-
+  faLinked = faLinkedin;
+  faFb = faFacebook;
+  fatwitter = faXTwitter;
+  faInstagram = faInstagram;
 }

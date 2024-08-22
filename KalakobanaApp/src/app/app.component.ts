03624c1f0d7bc13788ec39b9d220d70e984804b7
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-22 18:50:31 +0400

    technology

--- KalakobanaApp/src/app/app.component.ts
+++ KalakobanaApp/src/app/app.component.ts
@@ -4,13 +4,17 @@ import { NavbarComponent } from "./navbar/navbar.component";
 import { HeroComponent } from "./hero/hero.component";
 import { NewsletterComponent } from "./newsletter/newsletter.component";
 import { FooterComponent } from "./footer/footer.component";
+import { PlusesComponent } from "./pluses/pluses.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
+import { TechnologyComponent } from "./technology/technology.component";
+import { TeamComponent } from "./team/team.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent, HeroComponent, NewsletterComponent, FooterComponent]
+    imports: [RouterOutlet, NavbarComponent, HeroComponent, NewsletterComponent, FooterComponent, PlusesComponent, AboutUsComponent, TechnologyComponent, TeamComponent]
 })
 export class AppComponent {
   title = 'KalakobanaApp';

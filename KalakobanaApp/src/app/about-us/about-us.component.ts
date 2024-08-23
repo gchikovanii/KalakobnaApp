e61fa030fe72ce3c61c2eb0499d2e5ab8af73c7e
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-23 18:24:53 +0400

    About us button added

--- KalakobanaApp/src/app/about-us/about-us.component.ts
+++ KalakobanaApp/src/app/about-us/about-us.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
+  shortText: string = 'Through collaboration, diverse perspectives and strengths are leveraged to create inclusive environments where everyone has the opportunity to thrive. This approach not only fosters personal growth and achievement but also strengthens the fabric of society...';
+  fullText: string = this.shortText + ' Through collaboration, diverse perspectives and strengths are leveraged to create inclusive environments where everyone has the opportunity to thrive. This approach not only fosters personal growth and achievement but also strengthens the fabric of society.';
+  
+  isTextExpanded: boolean = false;
 
+  toggleText() {
+    this.isTextExpanded = !this.isTextExpanded;
+  }
 }

7c5bf6bf7f5bd2ea641d4058effc354d937dd111
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-23 19:31:17 +0400

    translater added

--- KalakobanaApp/src/app/app.component.ts
+++ KalakobanaApp/src/app/app.component.ts
@@ -8,15 +8,30 @@ import { PlusesComponent } from "./pluses/pluses.component";
 import { AboutUsComponent } from "./about-us/about-us.component";
 import { TechnologyComponent } from "./technology/technology.component";
 import { TeamComponent } from "./team/team.component";
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent, HeroComponent, NewsletterComponent, FooterComponent, PlusesComponent, AboutUsComponent, TechnologyComponent, TeamComponent],
+    imports: [RouterOutlet, NavbarComponent, HeroComponent, NewsletterComponent, FooterComponent, PlusesComponent, AboutUsComponent, TechnologyComponent, 
+      TranslateModule,TeamComponent],
 })
 export class AppComponent {
   title = 'KalakobanaApp';
-  
+
+  constructor(private translate: TranslateService) {
+    const savedLanguage = localStorage.getItem('language');
+    const defaultLanguage = savedLanguage ? savedLanguage : 'ka';
+    translate.setDefaultLang(defaultLanguage);
+    translate.use(defaultLanguage);
+  }
+
+  switchLanguage(lang: string) {
+    this.translate.use(lang).subscribe(() => {
+      localStorage.setItem('language', lang);
+    });
+  }
+
 }
--- KalakobanaApp/src/app/app.config.ts
+++ KalakobanaApp/src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core'
+import {TranslateHttpLoader} from '@ngx-translate/http-loader'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),  provideAnimations(),]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),  provideAnimations(),
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }))
+  ]
 };
+
+export function HttpLoaderFactory(http: HttpClient){
+  return new TranslateHttpLoader(http, './i18n/', '.json');
+
+}

6d6ee470b2673821799632a4c301012dbb81c51e
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-24 17:51:02 +0400

    Page not found

--- KalakobanaApp/src/app/app.routes.ts
+++ KalakobanaApp/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ContactComponent } from './contact/contact.component';
 import { GameHubComponent } from './game-hub/game-hub.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'game-hub', component: GameHubComponent },
+    { path: '**', component: NotFoundComponent }
 ];
